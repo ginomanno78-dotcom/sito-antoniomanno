bc44c76a3d9c7e5661dc81a513de27652d240552
ginomanno78-dotcom <ginomanno78@gmail.com>
2026-09-08 10:52:05 +0200

    cms: +1 colours (25–25)

--- novita-foto.js
+++ novita-foto.js
@@ -22,7 +22,7 @@
             { id: 'arti-mestieri', count: 19, publishedAt: '2026-09-08T10:40:47+02:00' },
             { id: 'fulvio-vellone', count: 27, publishedAt: '2026-08-11T16:40:00+02:00' },
             { id: 'urban', count: 2, publishedAt: '2026-08-17T14:10:00+02:00' },
-            { id: 'colours', count: 1, publishedAt: '2026-08-27T10:30:00+02:00' },
+            { id: 'colours', count: 2, publishedAt: '2026-09-08T10:52:02+02:00' },
             { id: 'landscapes', count: 1, publishedAt: '2026-08-27T10:30:00+02:00' },
             { id: 'country-market', count: 4, publishedAt: '2026-09-08T10:34:45+02:00' },
             { id: 'windows', count: 1, publishedAt: '2026-09-08T10:47:04+02:00' }
